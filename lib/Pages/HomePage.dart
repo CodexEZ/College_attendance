76c9359815874b6c75a825e711da977ab0c2c9d3
CodexEZ <pratapsinghaswin@gmail.com>
2022-11-16 21:46:21 +0530

    second commit

--- lib/Pages/HomePage.dart
+++ lib/Pages/HomePage.dart
@@ -1,12 +1,14 @@
-import 'dart:async';
 
+
+
+
+import 'package:path_provider/path_provider.dart';
+
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/cupertino.dart';
-import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
-import 'package:google_fonts/google_fonts.dart';
-import 'package:wifi_scan/wifi_scan.dart';
-
+import 'package:nearby_connections/nearby_connections.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
@@ -16,209 +18,291 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-  List<WiFiAccessPoint> accessPoints = <WiFiAccessPoint>[];
-  StreamSubscription<List<WiFiAccessPoint>>? subscription;
-  bool shouldCheckCan = true;
+  String? id = FirebaseAuth.instance.currentUser?.email;
+  final Strategy strategy = Strategy.P2P_STAR;
+  Map<String, ConnectionInfo> endpointMap = Map();
 
-  bool get isStreaming => subscription != null;
-
-  Future<void> _startScan(BuildContext context) async {
-    // check if "can" startScan
-    if (shouldCheckCan) {
-      // check if can-startScan
-      final can = await WiFiScan.instance.canStartScan();
-      // if can-not, then show error
-      if (can != CanStartScan.yes) {
-        if (mounted) kShowSnackBar(context, "Cannot start scan: $can");
-        return;
-      }
-    }
-
-    // call startScan API
-    final result = await WiFiScan.instance.startScan();
-    if (mounted) kShowSnackBar(context, "startScan: $result");
-    // reset access points.
-    setState(() => accessPoints = <WiFiAccessPoint>[]);
-  }
-
-  Future<bool> _canGetScannedResults(BuildContext context) async {
-    if (shouldCheckCan) {
-      // check if can-getScannedResults
-      final can = await WiFiScan.instance.canGetScannedResults();
-      // if can-not, then show error
-      if (can != CanGetScannedResults.yes) {
-        if (mounted) kShowSnackBar(context, "Cannot get scanned results: $can");
-        accessPoints = <WiFiAccessPoint>[];
-        return false;
-      }
-    }
-    return true;
-  }
-
-  Future<void> _getScannedResults(BuildContext context) async {
-    if (await _canGetScannedResults(context)) {
-      // get scanned results
-      final results = await WiFiScan.instance.getScannedResults();
-      setState(() => accessPoints = results);
-    }
-  }
-
-  Future<void> _startListeningToScanResults(BuildContext context) async {
-    if (await _canGetScannedResults(context)) {
-      subscription = WiFiScan.instance.onScannedResultsAvailable
-          .listen((result) => setState(() => accessPoints = result));
-    }
-  }
-
-  void _stopListeningToScanResults() {
-    subscription?.cancel();
-    setState(() => subscription = null);
-  }
-
-  @override
-  void dispose() {
-    super.dispose();
-    // stop subscription for scanned results
-    _stopListeningToScanResults();
-  }
-
-  // build toggle with label
-  Widget _buildToggle({
-    String? label,
-    bool value = false,
-    ValueChanged<bool>? onChanged,
-    Color? activeColor,
-  }) =>
-      Row(
-        children: [
-          if (label != null) Text(label, style: GoogleFonts.lato(textStyle: TextStyle(color: Colors.white)),),
-          Switch(value: value, onChanged: onChanged, activeColor: activeColor),
-        ],
-      );
-
+  String? tempFileUri; //reference to the file currently being transferred
+  Map<int, String> map =
+  Map();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.black,
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.power_settings_new),
+        onPressed: (){
+          FirebaseAuth.instance.signOut();
+        },
+      ),
       appBar: AppBar(
-        backgroundColor: Colors.white24,
-        title:  Text(
-          "Student List",
-          style: GoogleFonts.lato(textStyle: TextStyle(
-            color: Colors.white
-          )),
-        ),
+        backgroundColor: Colors.black,
+        title:Text("Permissions ->"),
         actions: [
-          _buildToggle(
-            label: "Scan Access",
-            value: shouldCheckCan,
-            onChanged: (v)=>setState(()=>shouldCheckCan=v),
-            activeColor: Colors.green
+          IconButton(
+            icon: Icon(Icons.gps_fixed),
+            onPressed:() async {
+              if (await Nearby().askLocationPermission()) {
+                ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+                    content: Text("Location Permission granted :)")));
+              } else {
+                ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+                    content:
+                    Text("Location permissions not granted :(")));
+              }
+            },
+          ),
+          IconButton(
+            icon: Icon(Icons.bluetooth),
+            onPressed: (){
+              Nearby().askBluetoothPermission();
+        },
+          ),
+          IconButton(
+            icon: Icon(Icons.pin_drop),
+            onPressed: ()async{
+              if (await Nearby().enableLocationServices()) {
+              ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+              content: Text("Location Service Enabled :)")));
+              } else {
+              ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+              content:
+              Text("Enabling Location Service Failed :(")));
+              }
+            },
           )
         ],
-      ),
-      body: Builder(
-        builder: (context)=>Padding(
-          padding: EdgeInsets.all(10),
-          child: Column(
-            children: [
-              Row(
-                children: [
-                  ElevatedButton.icon(
-                    style: ButtonStyle(
-                      backgroundColor:  MaterialStateProperty.all<Color>(Colors.white24)
-                    ),
-                    icon: Icon(Icons.wifi),
-                    label: Text(
-                      "SCAN",
-                      style: GoogleFonts.lato(
-                        textStyle: TextStyle(
-                          color: Colors.white
-                        )
-                      ),
-                    ),
-                    onPressed: () async=>_startScan(context),
-                  ),
-                  Expanded(child: SizedBox()),
-                  ElevatedButton.icon(
-                    style: ButtonStyle(
-                        backgroundColor:  MaterialStateProperty.all<Color>(Colors.white24)
-                    ),
-                    icon: Icon(Icons.refresh),
-                    label: Text(
-                      "Refresh",
-                      style: GoogleFonts.lato(
-                          textStyle: TextStyle(
-                              color: Colors.white
-                          )
-                      ),
-                    ),
-                    onPressed: () async=>_getScannedResults(context),
-                  ),
-                  Expanded(child: SizedBox()),
-                  _buildToggle(
-                    label: "Allow scan",
-                    value: isStreaming,
-                    onChanged: (shouldStream) async=> shouldStream ? await _startListeningToScanResults(context):_stopListeningToScanResults(),
-                    activeColor: Colors.green
-                  )
-                ],
-              ),
-              const Divider( color: Colors.white24,),
-              Flexible(
-                child: Center(
-                  child: accessPoints.isEmpty
-                  ? Text("No Results. Make sure wifi is on.", style: GoogleFonts.lato(textStyle: TextStyle(color: Colors.white)),)
-                  :ListView.builder(
-                    itemCount: accessPoints.length,
-                    itemBuilder: (context, i)=>
-                    _AccessPointTile(accessPoint: accessPoints[i]),
-                  ),
+        ),
+      body: Padding(
+        padding: EdgeInsets.symmetric(horizontal: 5),
+        child: Column(
+          children: [
+            Row(
+              // children: [
+              //   Expanded(child: SizedBox()),
+              //   CircleAvatar(
+              //     backgroundColor: Colors.red,
+              //     child: IconButton(
+              //       icon: Icon(Icons.arrow_upward),
+              //       onPressed: ()async {
+              //         try {
+              //           bool a = await Nearby().startAdvertising(
+              //             id!,
+              //             strategy,
+              //             onConnectionInitiated: onConnectionInit,
+              //             onConnectionResult: (id, status) {
+              //               showSnackbar(status);
+              //             },
+              //             onDisconnected: (id) {
+              //               showSnackbar(
+              //                   "Disconnected: ${endpointMap[id]!.endpointName}, id $id");
+              //               setState(() {
+              //                 endpointMap.remove(id);
+              //               });
+              //             },
+              //           );
+              //           showSnackbar("ADVERTISING: " + a.toString());
+              //         } catch (exception) {
+              //           showSnackbar(exception);
+              //         }
+              //       },
+              //     ),
+              //   ),
+              //   ElevatedButton(
+              //       child: Text("Stop Advertising"),
+              //       onPressed: () async {
+              //         await Nearby().stopAdvertising();
+              //       }
+              //   ),
+              //   Expanded(child: SizedBox())
+              // ],
+            ),
+            Row(
+              children: [
+                Expanded(child: SizedBox()),
+                ElevatedButton(
+                  child: Text("Start Discovery"),
+                  onPressed: () async {
+                    try {
+                      bool a = await Nearby().startDiscovery(
+                        id!,
+                        strategy,
+                        onEndpointFound: (id, name, serviceId) {
+                          // show sheet automatically to request connection
+                          showModalBottomSheet(
+                            context: context,
+                            builder: (builder) {
+                              return Center(
+                                child: Column(
+                                  children: <Widget>[
+                                    Text("id: " + id),
+                                    Text("Name: " + name),
+                                    Text("ServiceId: " + serviceId),
+                                    ElevatedButton(
+                                      child: Text("Request Connection"),
+                                      onPressed: () {
+                                        FirebaseFirestore.instance.collection('std').doc().set({
+                                          "attendance":id,
+                                          "check":true
+                                        }
+                                        );
+                                        Navigator.pop(context);
+                                        Nearby().requestConnection(
+                                          "FUCK",
+                                          id,
+                                          onConnectionInitiated: (id, info) {
+                                            onConnectionInit(id, info);
+                                          },
+                                          onConnectionResult: (id, status) {
+                                            showSnackbar(status);
+                                          },
+                                          onDisconnected: (id) {
+                                            setState(() {
+                                              endpointMap.remove(id);
+                                            });
+                                            showSnackbar(
+                                                "Disconnected from: ${endpointMap[id]!.endpointName}, id $id");
+                                          },
+                                        );
+                                      },
+                                    ),
+                                  ],
+                                ),
+                              );
+                            },
+                          );
+                        },
+                        onEndpointLost: (id) {
+                          showSnackbar(
+                              "Lost discovered Endpoint: ${endpointMap[id]!.endpointName}, id $id");
+                        },
+                      );
+                      showSnackbar("DISCOVERING: " + a.toString());
+                    } catch (e) {
+                      showSnackbar(e);
+                    }
+                  },
                 ),
-              )
-
-            ],
-          ),
+                Expanded(child: SizedBox()),
+                ElevatedButton(
+                  child: Text("Stop Discovery"),
+                  onPressed: () async {
+                    await Nearby().stopDiscovery();
+                  },
+                ),
+                Expanded(child: SizedBox()),
+              ],
+            ),
+          ],
         ),
       ),
+
     );
   }
-}
-class _AccessPointTile extends StatefulWidget {
-  final WiFiAccessPoint accessPoint;
+  void showSnackbar(dynamic a) {
+    ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+      content: Text(a.toString()),
+    ));
+  }
 
-  const _AccessPointTile({Key? key, required this.accessPoint})
-      : super(key: key);
+  Future<bool> moveFile(String uri, String fileName) async {
+    String parentDir = (await getExternalStorageDirectory())!.absolute.path;
+    final b =
+    await Nearby().copyFileAndDeleteOriginal(uri, '$parentDir/$fileName');
 
-  @override
-  State<_AccessPointTile> createState() => _AccessPointTileState();
-}
+    showSnackbar("Moved file:" + b.toString());
+    return b;
+  }
+  void onConnectionInit(String id, ConnectionInfo info) {
+    showModalBottomSheet(
+      context: context,
+      builder: (builder) {
+        return Center(
+          child: Column(
+            children: <Widget>[
+              Text("id: " + id),
+              Text("Token: " + info.authenticationToken),
+              Text("Name" + info.endpointName),
+              Text("Incoming: " + info.isIncomingConnection.toString()),
+              ElevatedButton(
+                child: Text("Accept Connection"),
+                onPressed: () {
+                  Navigator.pop(context);
+                  setState(() {
+                    endpointMap[id] = info;
+                  });
+                  Nearby().acceptConnection(
+                    id,
+                    onPayLoadRecieved: (endid, payload) async {
+                      if (payload.type == PayloadType.BYTES) {
+                        String str = String.fromCharCodes(payload.bytes!);
+                        showSnackbar(endid + ": " + str);
 
-class _AccessPointTileState extends State<_AccessPointTile> {
-  // build row that can display info, based on label: value pair.
-  bool check = false;
-  @override
-  Widget build(BuildContext context) {
-    final title = widget.accessPoint.ssid.isNotEmpty ? widget.accessPoint.ssid : "**EMPTY**";
-    final signalIcon = widget.accessPoint.level >= -80
-        ? Icons.signal_wifi_4_bar
-        : Icons.signal_wifi_0_bar;
-    return ListTile(
-      visualDensity: VisualDensity.compact,
-      leading: check?Icon(Icons.check,color: Colors.green,):Icon(signalIcon,color: Colors.white,),
-      title: Text(title,style: TextStyle(color: Colors.white),),
-      subtitle: Text(widget.accessPoint.capabilities),
-      onTap: (){
-        setState((){
-          check?check = false:check = true;
-        });
+                        if (str.contains(':')) {
+                          // used for file payload as file payload is mapped as
+                          // payloadId:filename
+                          int payloadId = int.parse(str.split(':')[0]);
+                          String fileName = (str.split(':')[1]);
+
+                          if (map.containsKey(payloadId)) {
+                            if (tempFileUri != null) {
+                              moveFile(tempFileUri!, fileName);
+                            } else {
+                              showSnackbar("File doesn't exist");
+                            }
+                          } else {
+                            //add to map if not already
+                            map[payloadId] = fileName;
+                          }
+                        }
+                      } else if (payload.type == PayloadType.FILE) {
+                        showSnackbar(endid + ": File transfer started");
+                        tempFileUri = payload.uri;
+                      }
+                    },
+                    onPayloadTransferUpdate: (endid, payloadTransferUpdate) {
+                      if (payloadTransferUpdate.status ==
+                          PayloadStatus.IN_PROGRESS) {
+                        print(payloadTransferUpdate.bytesTransferred);
+                      } else if (payloadTransferUpdate.status ==
+                          PayloadStatus.FAILURE) {
+                        print("failed");
+                        showSnackbar(endid + ": FAILED to transfer file");
+                      } else if (payloadTransferUpdate.status ==
+                          PayloadStatus.SUCCESS) {
+                        showSnackbar(
+                            "$endid success, total bytes = ${payloadTransferUpdate.totalBytes}");
+
+                        if (map.containsKey(payloadTransferUpdate.id)) {
+                          //rename the file now
+                          String name = map[payloadTransferUpdate.id]!;
+                          moveFile(tempFileUri!, name);
+                        } else {
+                          //bytes not received till yet
+                          map[payloadTransferUpdate.id] = "";
+                        }
+                      }
+                    },
+                  );
+                },
+              ),
+              ElevatedButton(
+                child: Text("Reject Connection"),
+                onPressed: () async {
+                  Navigator.pop(context);
+                  try {
+                    await Nearby().rejectConnection(id);
+                  } catch (e) {
+                    showSnackbar(e);
+                  }
+                },
+              ),
+            ],
+          ),
+        );
       },
     );
   }
+
 }
-void kShowSnackBar(BuildContext context, String message) {
-  if (kDebugMode) print(message);
-  ScaffoldMessenger.of(context)
-    ..hideCurrentSnackBar()
-    ..showSnackBar(SnackBar(content: Text(message)));
-}
+
